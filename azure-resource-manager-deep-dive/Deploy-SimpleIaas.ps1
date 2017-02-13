39d574953f1a4abe670bf4a9d07fe9d6370846ad
James Bannan <james@bannan.com.au>
2017-02-14 09:23:26 +1100

    Added files for Azure Resource Manager Deep Dive

---
+++ azure-resource-manager-deep-dive/Deploy-SimpleIaas.ps1
@@ -0,0 +1,69 @@
+﻿
+### Define Deployment Variables
+
+$resourceGroupName = 'pluralsight-arm-simple-iaas'
+$resourceProviderNamespace = 'Microsoft.Network'
+$resourceTypeName = 'virtualNetworks'
+$resourceGroupLocation = 'Australia Southeast'
+
+$vNetName = 'vnet-pluralsight'
+$vNetAddressPrefix = '172.16.0.0/16'
+$vNetSubnet1Name = 'subnet-1'
+$vNetSubnet1Prefix = '172.16.1.0/24'
+$vNetSubnet2Name = 'subnet-2'
+$vNetSubnet2Prefix = '172.16.2.0/24'
+
+$randomString = ([char[]]([char]'a'..[char]'z') + 0..9 | Sort-Object {Get-Random})[0..8] -join ''
+$storageAccountNamePrefix = 'storage'
+$storageAccountType = 'Standard_LRS'
+$storageAccountName = $storageAccountNamePrefix + ($storageAccountType.Replace('Standard_','')).ToLower() + $randomString
+
+### Get ARM Provider Locations
+{
+((Get-AzureRmResourceProvider `
+    -ProviderNamespace "$resourceProviderNamespace").ResourceTypes | `
+    Where-Object {$_.ResourceTypeName -eq "$resourceTypeName"}).Locations | `
+    Sort-Object
+}
+
+### Create ARM Resource Group
+{
+$resourceGroup = New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $resourceGroupLocation `
+    -Verbose -Force
+}
+
+### Create Virtual Network Subnets
+{
+$vNetSubnet1 = New-AzureRmVirtualNetworkSubnetConfig `
+    -Name $vNetSubnet1Name `
+    -AddressPrefix $vNetSubnet1Prefix `
+    -Verbose
+
+$vNetSubnet2 = New-AzureRmVirtualNetworkSubnetConfig `
+    -Name $vNetSubnet2Name `
+    -AddressPrefix $vNetSubnet2Prefix `
+    -Verbose
+}
+
+### Create Virtual Network
+{
+$vNet = New-AzureRmVirtualNetwork `
+    -ResourceGroupName $resourceGroup.ResourceGroupName `
+    -Location $resourceGroup.Location `
+    -Name $vNetName `
+    -AddressPrefix $vNetAddressPrefix `
+    -Subnet $vNetSubnet1,$vNetSubnet2 `
+    -Verbose -Force
+}
+
+### 
+{
+$storageAccount = New-AzureRmStorageAccount `
+    -ResourceGroupName $resourceGroup.ResourceGroupName `
+    -Location $resourceGroup.Location `
+    -Name $storageAccountName `
+    -Type $storageAccountType `
+    -Verbose
+}
