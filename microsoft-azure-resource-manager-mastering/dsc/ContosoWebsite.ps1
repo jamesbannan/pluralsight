3e331e8f4592c71d900690c08025eb73df7794ad
James Bannan <james@bannan.com.au>
2017-05-29 10:21:14 +1000

    Added ContosoWebsite.ps1 DSC script

---
+++ microsoft-azure-resource-manager-mastering/dsc/ContosoWebsite.ps1
@@ -0,0 +1,27 @@
+Configuration ContosoWebsite
+{
+  param ($MachineName)
+
+  Node $MachineName
+  {
+    #Install the IIS Role
+    WindowsFeature IIS
+    {
+      Ensure = “Present”
+      Name = “Web-Server”
+    }
+
+    #Install ASP.NET 4.5
+    WindowsFeature ASP
+    {
+      Ensure = “Present”
+      Name = “Web-Asp-Net45”
+    }
+
+     WindowsFeature WebServerManagementConsole
+    {
+        Name = "Web-Mgmt-Console"
+        Ensure = "Present"
+    }
+  }
+} 
