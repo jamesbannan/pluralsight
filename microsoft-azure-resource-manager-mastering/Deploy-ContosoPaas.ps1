781f08f6a1f34afc04b41eaa7270ed730e85f2c0
James Bannan <james@bannan.com.au>
2017-06-21 11:49:56 +1000

    Included PaaS deployment resources

---
+++ microsoft-azure-resource-manager-mastering/Deploy-ContosoPaas.ps1
@@ -0,0 +1,26 @@
+﻿### Define Deployment Variables
+{
+$resourceGroupLocation = 'Australia Southeast'
+$resourceGroupName = 'contoso-paas'
+$resourceDeploymentName = 'contoso-paas-deployment'
+$templatePath = $env:SystemDrive + '\' + 'pluralsight'
+$templateFile = 'contosoPaas.json'
+$template = $templatePath + '\' + $templateFile
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    -Verbose -Force
+}
