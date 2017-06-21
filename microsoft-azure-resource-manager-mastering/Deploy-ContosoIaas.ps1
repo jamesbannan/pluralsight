8b4c5ddd0ebf03b09ce06730b2938e8c50b1d759
James Bannan <james@bannan.com.au>
2017-06-21 11:49:45 +1000

    Included IaaS deployment resources

---
+++ microsoft-azure-resource-manager-mastering/Deploy-ContosoIaas.ps1
@@ -0,0 +1,33 @@
+﻿### Define Deployment Variables
+{
+$location = 'Australia Southeast'
+$resourceGroupName = 'contoso-iaas'
+$resourceDeploymentName = 'contoso-iaas-deployment'
+$templatePath = $env:SystemDrive + '\' + 'pluralsight'
+$templateFile = 'contosoIaas.json'
+$template = $templatePath + '\' + $templateFile
+$password = "C0nts0sP@55"
+$securePassword = $password | ConvertTo-SecureString -AsPlainText -Force
+}
+
+### Create Resource Group
+{
+New-AzureRmResourceGroup `
+    -Name $resourceGroupName `
+    -Location $location `
+    -Verbose -Force
+}
+
+### Deploy Resources
+{
+
+$additionalParameters = New-Object -TypeName Hashtable
+$additionalParameters['vmAdminPassword'] = $securePassword
+
+New-AzureRmResourceGroupDeployment `
+    -Name $resourceDeploymentName `
+    -ResourceGroupName $resourceGroupName `
+    -TemplateFile $template `
+    @additionalParameters `
+    -Verbose -Force
+}
