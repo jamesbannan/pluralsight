dfbc192a595308196a95d669983e998c96ce4546
James Bannan <james@bannan.com.au>
2017-06-21 11:55:57 +1000

    Script to add Tags to VMs

---
+++ microsoft-azure-resource-manager-mastering/New-vmWebTags.ps1
@@ -0,0 +1,23 @@
+﻿
+### Get vmWeb VMs
+{
+
+$VMs = Get-AzureRmResource | `
+    Where-Object {$_.Name -like "vmWeb-*" -and $_.ResourceType -eq 'Microsoft.Compute/virtualMachines'}
+
+}
+
+### Add Tags to Resources
+{
+
+$tags = New-Object -TypeName Hashtable
+$tags += @{"department"="IT"}
+$tags += @{"environment"="Production"}
+
+foreach($VM in $VMs){
+    Set-AzureRmResource `
+        -Tag $tags `
+        -ResourceId $VM.ResourceId
+}
+
+}
