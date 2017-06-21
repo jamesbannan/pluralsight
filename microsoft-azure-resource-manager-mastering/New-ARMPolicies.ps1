c5c2bfd89c7cfa568f0125a009863423c887cf2c
James Bannan <james@bannan.com.au>
2017-06-21 11:55:45 +1000

    resources to create ARM policies

---
+++ microsoft-azure-resource-manager-mastering/New-ARMPolicies.ps1
@@ -0,0 +1,23 @@
+﻿
+### Create and Apply Location Policy
+{
+
+$policyName = 'policyLocationDefinition'
+$policyAssignment = 'policyLocationAssignment'
+$policyFile = $env:SystemDrive + '\' + 'pluralsight' + '\' + 'policylocation.json'
+
+New-AzureRmPolicyDefinition `
+    -Name $policyName `
+    -Policy $policyFile `
+    -Verbose
+
+$resourceGroup = Get-AzureRmResourceGroup -Name 'contoso-paas'
+$policy = Get-AzureRmPolicyDefinition -Name $policyName
+
+New-AzureRmPolicyAssignment `
+    -Name $policyAssignment `
+    -PolicyDefinition $policy `
+    -Scope $resourceGroup.ResourceId `
+    -Verbose
+
+}
