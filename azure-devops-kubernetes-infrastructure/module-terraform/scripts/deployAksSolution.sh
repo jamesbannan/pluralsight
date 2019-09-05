b026cb0535820639e06e64a8810dd9f1f6715618
James Bannan <james@bannan.com.au>
2019-09-05 16:55:51 +1000

    Script to deploy TF solution

---
+++ azure-devops-kubernetes-infrastructure/module-terraform/scripts/deployAksSolution.sh
@@ -0,0 +1,25 @@
+# Get Remote State Storage Account details
+tfstateRg='demo-tfstate'
+tfstateAccount=$(az storage account list --resource-group ${tfstateRg} | jq .[0])
+tfstateAccountName=$(echo $tfstateAccount | jq .name -r)
+tfstateAccountKey=$(az storage account keys list --resource-group ${tfstateRg} --account-name ${tfstateAccountName} | jq .[0].value -r)
+tfstateContainer='tfstate'
+tfstateKeyName='key=pluralsight-aks.tfstate'
+
+### Deploy AKS Solution
+# Initialise solution
+terraform init \
+    -backend-config="storage_account_name=${tfstateAccountName}" \
+    -backend-config="container_name=${tfstateContainer}" \
+    -backend-config="access_key=${tfstateAccountKey}" \
+    -backend-config="${tfstateKeyName}"
+
+# Validate solution
+terraform validate
+
+# Plan solution deployment
+planName='aks-cluster.plan'
+terraform plan -out=${planName}
+
+# Deploy solution
+terraform apply -auto-approve ${planName}
