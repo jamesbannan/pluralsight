704233b118a728d38812e91841bb7928a9089214
James Bannan <james@bannan.com.au>
2019-09-05 12:15:02 +1000

    Script to deploy AKS cluster and resources

---
+++ azure-devops-kubernetes-infrastructure/scripts/deployAks.sh
@@ -0,0 +1,39 @@
+# Check Resource Provider registration
+namespace='Microsoft.ContainerService'
+if [ "$(az provider show --namespace ${namespace} | jq -r .registrationState)" != 'Registered' ]
+then
+      az provider register --namespace ${namespace} --verbose
+else
+      echo "Namespace \"${namespace}\" is already registered."
+fi
+
+# Create Resource Group
+groupName='demo-aks'
+groupLocation='Australia East'
+group=$(az group create --name ${groupName} --location "${groupLocation}" --verbose)
+
+# Deploy Log Analytics Workspace
+solution='logAnalytics'
+templatePath='../arm'
+templateFile="${templatePath}/${solution}/azureDeploy.json"
+
+timestamp=$(date -u +%FT%TZ | tr -dc '[:alnum:]\n\r')
+name="$(echo $group | jq .name -r)-${timestamp}"
+deployment=$(az group deployment create --resource-group $(echo $group | jq .name -r) --name ${name} --template-file ${templateFile} --verbose)
+
+# Deploy basic AKS environment
+clusterName='demoCluster'
+logAnalyticsId=$(echo $deployment | jq .properties.outputs.workspaceResourceId.value -r)
+az aks create \
+    --resource-group $(echo $group | jq .name -r) \
+    --location $(echo $group | jq .location -r) \
+    --name ${clusterName} \
+    --node-count 1 \
+    --node-vm-size Standard_DS1_v2 \
+    --enable-addons monitoring \
+    --generate-ssh-keys \
+    --workspace-resource-id ${logAnalyticsId} \
+    --verbose
+
+# Get AKS Credentials
+az aks get-credentials --resource-group $(echo $group | jq .name -r) --name ${clusterName}
