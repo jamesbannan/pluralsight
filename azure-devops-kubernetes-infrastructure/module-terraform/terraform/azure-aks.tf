d3daaae495b98e1292f55906f3b9e0cc407b0214
James Bannan <james@bannan.com.au>
2019-09-06 10:59:28 +1000

    Adjusted node count to demo AKS scaling with TF

--- azure-devops-kubernetes-infrastructure/module-terraform/terraform/azure-aks.tf
+++ azure-devops-kubernetes-infrastructure/module-terraform/terraform/azure-aks.tf
@@ -14,7 +14,7 @@ resource "azurerm_kubernetes_cluster" "demo" {
 
     agent_pool_profile {
         name            = "nodepool01"
-        count           = "2"
+        count           = "1"
         vm_size         = "Standard_DS1_v2"
         os_type         = "Linux"
         os_disk_size_gb = 30
