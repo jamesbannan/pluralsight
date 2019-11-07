cf2c22dd9d5e237d1aae2319a88f1a74a246950d
James Bannan <james@bannan.com.au>
2019-11-07 14:54:37 +1100

    Refactored and added example

--- chef-fluency/chef-repo/cookbooks/node-info/recipes/node-info.rb
+++ chef-fluency/chef-repo/cookbooks/node-info/recipes/node-info.rb
@@ -4,8 +4,16 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
+folder = '/'
 filename = 'node-info.txt'
+content = 'This is a test.'
 
-file "/#{filename}" do
-  content 'This is a test.'
+file "#{folder}#{filename}" do
+  content "#{content}"
 end
+
+#if platform_family?("debian")
+#  file "#{folder}#{filename}" do
+#    content "#{content}"
+#  end
+#end
