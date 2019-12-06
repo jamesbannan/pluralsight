0ca3060bb51be662f6bb54a58a8aed7e93d5a6ec
James Bannan <james@bannan.com.au>
2019-12-07 09:16:05 +1100

    Updated JQ handling to support zsh

--- ai-engineer-deploying-solutions/module-iot/scripts/deployIotSolution.sh
+++ ai-engineer-deploying-solutions/module-iot/scripts/deployIotSolution.sh
@@ -9,9 +9,9 @@ done
 
 # Get Remote State Storage Account details
 tfstateRg='iot-tfstate'
-tfstateAccount=$(az storage account list --resource-group ${tfstateRg} | jq .[0])
+tfstateAccount=$(az storage account list --resource-group ${tfstateRg} | jq '[.[]][0]')
 tfstateAccountName=$(echo $tfstateAccount | jq .name -r)
-tfstateAccountKey=$(az storage account keys list --resource-group ${tfstateRg} --account-name ${tfstateAccountName} | jq .[0].value -r)
+tfstateAccountKey=$(az storage account keys list --resource-group ${tfstateRg} --account-name ${tfstateAccountName} | jq '[.[]][0].value' -r)
 tfstateContainer='tfstate'
 tfstateKeyName='key=pluralsight-iot.tfstate'
 
