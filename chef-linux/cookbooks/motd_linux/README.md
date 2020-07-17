5b7d3900b042524e44a90b6599b7bede31ced20f
James Bannan <james@bannan.com.au>
2020-07-17 16:56:31 +1000

    Initial motd_linux cookbook

---
+++ chef-linux/cookbooks/motd_linux/README.md
@@ -0,0 +1,4 @@
+# motd_linux
+
+TODO: Enter the cookbook description here.
+
---
+++ chef-linux/cookbooks/motd_linux/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: motd_linux
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
