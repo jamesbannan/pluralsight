33cbb75ee7b40b0841cd8c159f51a0c9925144aa
James Bannan <james@bannan.com.au>
2020-08-09 07:01:41 +1000

    Base assets for windows_packages cookbook

---
+++ chef-windows/cookbooks/windows_packages/spec/unit/recipes/default_spec.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook:: windows_packages
+# Spec:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'windows_packages::default' do
+  context 'When all attributes are default, on Ubuntu 20.04' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'ubuntu', '20.04'
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+
+  context 'When all attributes are default, on CentOS 8' do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+    platform 'centos', '8'
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
