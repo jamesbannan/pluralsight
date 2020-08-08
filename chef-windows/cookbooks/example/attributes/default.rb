dff81ffab33bbcb78b8f08b4572ce23c7ae47fa4
James Bannan <james@bannan.com.au>
2020-08-09 06:58:01 +1000

    Removed example cookbook

--- chef-windows/cookbooks/example/attributes/default.rb
+++
@@ -1,7 +0,0 @@
-# This is a Chef Infra Client attributes file. It can be used to specify default
-# and override attributes to be applied to nodes that run this cookbook.
-
-# Set a default name
-default['example']['name'] = 'Sam Doe'
-
-# For further information, see the Chef Infra documentation (https://docs.chef.io/attributes/).
--- chef-windows/cookbooks/example/recipes/default.rb
+++
@@ -1,8 +0,0 @@
-# This is a Chef Infra Client recipe file. It can be used to specify resources
-# which will apply configuration to a server.
-
-log "Welcome to Chef Infra Client, #{node['example']['name']}!" do
-  level :info
-end
-
-# For more information, see the documentation: https://docs.chef.io/recipes
