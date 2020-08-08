18c4e9dde3f9169f8677df260869fb4778d673b1
James Bannan <james@bannan.com.au>
2020-08-08 16:37:55 +1000

    Corrected deprecated Fauxhai version reference

--- chef-fluency/chef-repo/cookbooks/helloworld/spec/unit/recipes/default_spec.rb
+++ chef-fluency/chef-repo/cookbooks/helloworld/spec/unit/recipes/default_spec.rb
@@ -24,7 +24,7 @@ describe 'helloworld::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7')
       runner.converge(described_recipe)
     end
 
