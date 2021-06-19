53477db12e0ffb6bf0a6094306404c3f125d4721
James Bannan <james@bannan.com.au>
2021-06-19 18:21:59 +1000

    Added comments

--- mastering-azure-resource-manager-architecture/module-04/cli/deployPaasSolution-01.sh
+++ mastering-azure-resource-manager-architecture/module-04/cli/deployPaasSolution-01.sh
@@ -24,23 +24,26 @@ done
 
 ### Create App Service Plans
 for i in "${locations[@]}"; do
+    # Define Deployment Variables
     resourceGroupName="${appNamePrefix}-paas-${i}"
     resourceGroup=$(az group show --name ${resourceGroupName} --output json)
     resourceGroupLocation=$(echo $resourceGroup | jq .location -r)
 
     appServicePlanName="${appNamePrefix}-plan-${resourceGroupLocation}"
 
+    # Deploy App Service Plans
     az appservice plan create \
         --resource-group $(echo $resourceGroup | jq .name -r) \
         --name ${appServicePlanName} \
         --location $(echo $resourceGroup | jq .location -r) \
         --sku S1 \
         --is-linux \
-        --output table
+        --output none
 done
 
 ### Create Web Apps
 for i in "${locations[@]}"; do
+    # Define Deployment Variables
     resourceGroupName="${appNamePrefix}-paas-${i}"
     resourceGroup=$(az group show --name ${resourceGroupName} --output json)
     resourceGroupLocation=$(echo $resourceGroup | jq .location -r)
@@ -50,6 +53,7 @@ for i in "${locations[@]}"; do
     webAppName="${appNamePrefix}-web-${resourceGroupLocation}-${nameSuffix}"
     appServicePlanName="${appNamePrefix}-plan-${resourceGroupLocation}"
     
+    # Deploy Web Apps
     az webapp create \
         --resource-group $(echo $resourceGroup | jq .name -r) \
         --name ${webAppName} \
